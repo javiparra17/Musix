84efa7aec2df524a2809bcb09e4f0681c91af380
Javier Parra <javierparraoyola619@hotmail.com>
2018-03-12 19:39:06 +0100

    Modificación settings.py

--- Musix/Musix/settings.py
+++ Musix/Musix/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Musix/images'),]
-STATIC_URL = '/images/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tracks')
 MEDIA_URL = '/tracks/'
