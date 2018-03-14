c3109d200e4b64f9016b7c0aafa89fdea40261a8
Javier Parra <javierparraoyola619@hotmail.com>
2018-03-14 17:05:58 +0100

    Modificación de settings y urls para la internacionalización

--- Musix/Musix/settings.py
+++ Musix/Musix/settings.py
@@ -104,9 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'es-ES'
+LANGUAGE_CODE = 'en-us'
+_ = lambda s: s
 
-TIME_ZONE = 'Europe/Madrid'
+LANGUAGES = (
+ ('es', _('Spanish')),
+ ('en', _('English')),
+)
+
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -114,6 +120,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+ os.path.join(BASE_DIR, "locale"),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
