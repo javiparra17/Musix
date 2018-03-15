8a92224b22357e55571a054473d1df46e0d97b71
Javier Parra <javierparraoyola619@hotmail.com>
2018-03-15 14:18:22 +0100

    Controlador de creación de usuario

--- Musix/main/views.py
+++ Musix/main/views.py
@@ -2,24 +2,36 @@ from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from forms import *
+from models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 def index(request):
     return render(request, "index.html")
 
 def createAccount(request):
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('index.html')
     if request.method == 'POST':
-        form = PersonForm(request.POST)
+        form = MusicianForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('index.html')
+            if form.cleaned_data['password'] == form.cleaned_data['password2']:
+                user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
+                user.first_name = form.cleaned_data['name']
+                user.last_name = form.cleaned_data['surname']
+                Musician.objects.create(user=user, gender=form.cleaned_data['gender'], description=form.cleaned_data['description'],
+                                        country=form.cleaned_data['country'], photo=form.cleaned_data['photo'], city=form.cleaned_data['city'])
+                return HttpResponseRedirect('index.html')
+            else:
+                error = "The passwords are not the same"
+                return render_to_response('createAccount.html', {'form': form, 'error': error})
     else:
-        form = PersonForm()
+        form = MusicianForm()
 
-    return render(request, 'createAccount.html', {'form':form })
+    return render_to_response('createAccount.html', {'form':form})
 
 #@login_required(login_url='/login.html')
 def createSong(request):
