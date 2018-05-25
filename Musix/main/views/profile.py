bbc9d685bc7db77ad41fcdd12afbe93eafd57b29
Javier Parra <javierparraoyola619@hotmail.com>
2018-05-25 13:36:53 +0200

    Funcionalidad, HTML y CSS de perfil de usuario

--- Musix/main/views/profile.py
+++ Musix/main/views/profile.py
@@ -1,4 +1,4 @@
-from main.models import Musician
+from main.models import Musician, Track, Song
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 
@@ -7,4 +7,12 @@ def profile(request, musician_username):
     user = get_object_or_404(User, username=musician_username)
     musician = get_object_or_404(Musician, user=user)
 
-    return render(request, 'profile.html', {'musician': musician})
+    tracks = Track.objects.filter(musician=musician, status="A")
+    finished_songs = Song.objects.filter(finished=True)
+    accepted_tracks = []
+    for t in tracks:
+        if t.song in finished_songs:
+            accepted_tracks.append(t)
+
+    return render(request, 'profile.html', {'musician': musician,
+                                            'tracks': accepted_tracks})
